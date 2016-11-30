31f1f259afb6157e3ac1bc9d68e65ec303f630bc
JonesVG <jonesvg@gmail.com>
2016-11-29 22:21:15 -0200

    First Code Commit with the android Git Public Repositories viewer.

---
+++ ProjetosAndroid/AndroidAppGuit/MainActivity.cs
@@ -0,0 +1,22 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace AndroidAppGuit
+{
+    [Activity(Label = "AndroidAppGuit", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        public int count = 1;
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            SetContentView(Resource.Layout.Main);            
+
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
---
+++ ProjetosAndroid/GuitHubApi/GuitHubApi.Droid/MainActivity.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace GuitHubApi.Droid
+{
+	[Activity (Label = "GuitHubApi.Droid", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : Activity
+	{
+		
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+
+            var txtUser = FindViewById<EditText>(Resource.Id.txtUser);
+            var btnSearch = FindViewById<Button>(Resource.Id.btnSearch);
+            var lvwResources = FindViewById<ListView>(Resource.Id.lvwRepositories);
+
+            btnSearch.Click += async (object sender, EventArgs e) =>
+            {
+                var gitHub = new GitHubAPI();
+                var repositories = await gitHub.GetAsync(txtUser.Text);
+                lvwResources.Adapter = new ArrayAdapter(this, Android.Resource.Layout.SimpleListItemSingleChoice, repositories);
+
+
+            };
+
+        }
+
+        
+    }
+}
+
+
